dbad4c1539b71f0b3019494fc693edcaf2e2c415
soumya098 <soumyarj904@gmail.com>
2020-09-02 00:46:35 +0530

    change 4

--- JS/clock.js
+++ JS/clock.js
@@ -3,7 +3,7 @@ function date_time(){
     let a= new Date();
     let time=a.toLocaleTimeString();
     let date=a.toDateString();
-    document.getElementById("time-display").innerHTML=time+" <br/>on "+date;
+    document.getElementById("time-display").innerHTML=time+" on "+date;
 }
 
 setInterval(date_time,1000);
