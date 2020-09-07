781a63a141e1fcaa21e54a64fc5cb0fa80d8fa1e
soumya098 <soumyarj904@gmail.com>
2020-09-08 04:11:06 +0530

    media queries added

--- JS/bjack.js
+++ JS/bjack.js
@@ -1,4 +1,12 @@
-document.querySelector("#hit").addEventListener("click",Btn_Hit);
-function Btn_Hit(){
-    alert("ouch");
+let BlackJackGame = {
+    "you": { "span": "#your-score", "div": "#your-box", "score": 0 },
+    "dealer": { "span": "#dealer-score", "div": "#dealer-box", "score": 0 }
+}
+let you=BlackJackGame.you;
+let dealer=BlackJackGame.dealer;
+document.querySelector("#hit").addEventListener("click", Btn_Hit);
+function Btn_Hit() {
+   let cardimage=document.createElement("img");
+   cardimage.src="images/bjackimages/2.png";
+   document.querySelector(you.div).appendChild(cardimage);
 }
